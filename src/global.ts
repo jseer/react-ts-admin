efb5114b4831ddaf24d8b81a75b07e85f78d757b
jseer <846336155@qq.com>
2023-05-07 16:13:19 +0800

    pv

--- src/global.ts
+++ src/global.ts
@@ -1,7 +1,14 @@
 import { ConfigProvider } from 'antd';
+import BrowserTracer from '@zjseer/browser-tracer';
 
 // antd
 ConfigProvider.config({
   prefixCls: 'admin',
   iconPrefixCls: 'adminicon',
 });
+
+BrowserTracer.init({
+  projectId: 1,
+  debug: true,
+  dsn: 'http://119.29.155.42/api/trace/send',
+})
---
+++ src/typing.d.ts
@@ -0,0 +1,6 @@
+declare module '@zjseer/browser-tracer' {
+    declare namespace BrowserTracer {
+        export function init(config: any){}
+    }
+    export default BrowserTracer;
+}
